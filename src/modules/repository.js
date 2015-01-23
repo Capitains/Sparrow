578a5b62f5256af6bf12ed95d9b87e66820b9c48
Thibault Clérice <leponteineptique@gmail.com>
2015-01-23 17:57:28 +0100

    Good parsing

--- src/modules/repository.js
+++ src/modules/repository.js
@@ -88,7 +88,7 @@ var _load = function(url, callback) {
 
   this.xhr("GET", url[0], function(data) {
 
-    _this.data[url[0]] = new _this.Repository(data, _this.namespace);
+    _this.data.push(new _this.TextInventory(data, _this.namespace, url[0]));
     if(url.length === 1) {
       callback();
     } else {
@@ -100,18 +100,43 @@ var _load = function(url, callback) {
   return this;
 }
 
-var TextGroupCTS3 = function(nodes) {
-  var o = {};
-  o.label = {};
+var TextCTS3 = function(nodes, type) {
+  var object = {};
+  object.type = type;
+  object.descriptions = {};
+  object.urn = "";
 
-  // We get the labels
-  [].map.call(nodes.getElementsByTagName("groupname"), function(groupname) {
-    o.defaultLang = groupname.getAttribute("xml:lang");
-    o.label[o.defaultLang] = groupname.textContent;
+    // We get the labels
+  [].map.call(nodes.getElementsByTagName("description"), function(groupname) {
+    object.defaultLang = groupname.getAttribute("xml:lang");
+    object.descriptions[object.defaultLang] = groupname.textContent;
   });
 
   // We create a function to have a name
-  o.getName = function(lang) {
+  object.getDesc = function(lang) {
+    if(lang === "undefined") {
+      lang = this.defaultLang;
+    } else if (!(lang in this.descriptions)) {
+      return this.descriptions[this.defaultLang];
+    }
+    return this.descriptions[lang];
+  }
+
+  return object;
+}
+
+var WorkCTS3 = function(nodes) {
+  var object = {};
+  object.label = {};
+
+  // We get the labels
+  [].map.call(nodes.getElementsByTagName("title"), function(groupname) {
+    object.defaultLang = groupname.getAttribute("xml:lang");
+    object.label[object.defaultLang] = groupname.textContent;
+  });
+
+  // We create a function to have a name
+  object.getTitle = function(lang) {
     if(lang === "undefined") {
       lang = this.defaultLang;
     } else if (!(lang in this.label)) {
@@ -119,55 +144,113 @@ var TextGroupCTS3 = function(nodes) {
     }
     return this.label[lang];
   }
-  return o;
+
+  object._Translation = function(dom) { return TextCTS3(dom, "translation")};
+  object._Edition = function(dom) { return TextCTS3(dom, "edition")};
+
+  object.editions = [].map.call(nodes.getElementsByTagName("edition"), object._Edition);
+  object.translations = [].map.call(nodes.getElementsByTagName("translation"), object._Translation);
+
+  object.texts = object.translations.concat(object.editions);
+
+  return object;
 }
 
-var InventoryCTS3 = function (xml, namespace) {
-  var o = {},
+var TextGroupCTS3 = function(nodes) {
+  var object = {};
+  object.label = {};
+
+  // We get the labels
+  [].map.call(nodes.getElementsByTagName("groupname"), function(groupname) {
+    object.defaultLang = groupname.getAttribute("xml:lang");
+    object.label[object.defaultLang] = groupname.textContent;
+  });
+
+  // We create a function to have a name
+  object.getName = function(lang) {
+    if(lang === "undefined") {
+      lang = this.defaultLang;
+    } else if (!(lang in this.label)) {
+      return this.label[this.defaultLang];
+    }
+    return this.label[lang];
+  }
+
+  object._Work = WorkCTS3;
+
+  object.works = [].map.call(
+    nodes.getElementsByTagName("work"),
+    object._Work
+  );
+
+  return object;
+}
+
+var TextInventoryCTS3 = function (xml, namespace, uri) {
+  var object = {},
       ti;
-  o.namespace = namespace;
-  o.TextGroup = TextGroupCTS3;
-  ti = xml.getElementsByTagNameNS(o.namespace, "TextInventory");
+
+  object.identifier = uri;
+  object.namespace = namespace;
+  ti = xml.getElementsByTagNameNS(object.namespace, "TextInventory");
+
+  object._TextGroup = TextGroupCTS3;
 
   if(ti.length == 1) {
-    o.TextInventory = ti[0];
-    o.TextGroups = [].map.call(o.TextInventory.getElementsByTagNameNS(o.namespace, "textgroup"), TextGroupCTS3);//
-    //.map(TextGroupCTS3)
+    object.textgroups = [].map.call(ti[0].getElementsByTagNameNS(object.namespace, "textgroup"), object._TextGroup);
   } else {
-    o.TextInventory = null;
-    o.TextInventory = [];
+    object.textgroups = [];
+  }
+
+  object.getRaw = function(lang) {
+    var r = {};
+    object.textgroups.forEach(function(tg) {
+      var tgLabel = tg.getName(lang);
+      r[tgLabel] = {};
+      tg.works.forEach(function(w) {
+        var wLabel = w.getTitle(lang);
+        r[tgLabel][wLabel] = {"editions" : {}, "translations" : {}};
+        w.editions.forEach(function(e) {
+          r[tgLabel][wLabel]["editions"][e.getDesc(lang)] = e.urn;
+        });
+        w.translations.forEach(function(t) {
+          r[tgLabel][wLabel]["translations"][t.getDesc(lang)] = t.urn;
+        });
+      });
+
+    });
+    return r;
   }
 
 
-  return o;
+  return object;
 }
 
 // Creating namespace for the retriever.
 function repository(version, namespace) {
-    var o;
-    o = {}
+  var object = {};
     if(typeof namespace === "undefined") {
       if(version === 3) {
-        o.namespace = "http://chs.harvard.edu/xmlns/cts3/ti";
+        object.namespace = "http://chs.harvard.edu/xmlns/cts3/ti";
       } else {
-        o.namespace = "http://chs.harvard.edu/xmlns/cts";
+        object.namespace = "http://chs.harvard.edu/xmlns/cts";
       }
     }
-    o.version = version;
-    o.url = [];
-    o.data = {};
-    o.setUrl = _setUrl;
-    o.load = _load;
-    o.xhr = _xhr;
+    object.version = version;
+    object.url = [];
+    object.data = [];
+    object.setUrl = _setUrl;
+    object.load = _load;
+    object.xhr = _xhr;
 
-    if (o.version === 3) {
-      o.Repository = InventoryCTS3;
+    if (object.version === 3) {
+      object.TextInventory = TextInventoryCTS3;
     } else {
-      o.Repository = null; // NotImplementedYet
+      object.Repository = null; // NotImplementedYet
     }
     
 
-    return o;
+    return object;
 }
 
   CTS.repository = repository;
