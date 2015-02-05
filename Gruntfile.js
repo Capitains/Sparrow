d07af2ae9bfefeb1449ebe0a51cfc9146d63dc27
Thibault Clérice <leponteineptique@gmail.com>
2015-02-05 10:19:31 +0100

    Removed unused dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,18 +64,9 @@ module.exports = function(grunt) {
   });
 
   // Register tasks.
-  grunt.loadNpmTasks('grunt-contrib-requirejs');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-compress');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-autoprefixer');
   grunt.loadNpmTasks('grunt-jslint');
 
   // Default task. 
