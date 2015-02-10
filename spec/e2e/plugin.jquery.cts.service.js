a56fd731b847c3c102b07951c1060b64d6373303
Thibault Clerice <leponteineptique@gmail.com>
2015-02-10 19:40:39 +0100

    MOAR TESTS

--- spec/e2e/plugin.jquery.cts.service.js
+++ spec/e2e/plugin.jquery.cts.service.js
@@ -11,26 +11,26 @@ describe('jQuery CTS Selector', function() {
       CTS.service._service.call(this, endpoint, options);
       this.method = "POST";
       this.options = {
-        "xml" : {
+        "checkbox" : {
           "type" : "boolean",
           "html" : "checkbox",
           "default" : false
         },
-        "inline" : {
+        "boolean" : {
           "type" : "boolean",
           "html" : "hidden",
           "default" : true
         },
-        "text" : {
+        "textarea" : {
           "type" : "text", // Text unlinke string is a big thing
           "html" : "textarea"
         },
-        "remove_node" : {
+        "list" : {
           "type" : "list",
           "html" : "input",
           "default" : ["teiHeader","head","speaker","note","ref"]
         },
-        "go_to_root" : {
+        "input" : {
           "type" : "string",
           "html" : "input",
           "default" : "TEI"
@@ -47,7 +47,7 @@ describe('jQuery CTS Selector', function() {
           "html" : "checkbox",
           "default" : true
         },
-        "hidden" : {
+        "boolean" : {
           "type" : "boolean",
           "html" : "hidden",
           "default" : false
@@ -97,6 +97,7 @@ describe('jQuery CTS Selector', function() {
     fixture.append(fixture)
     $j("body").append(fixture);
     fixture.ctsService(serviceName, options)
+    instance = fixture.data("_cts_service_"+serviceName);
   }
 
   var startFake = function(options) {
@@ -122,61 +123,176 @@ describe('jQuery CTS Selector', function() {
   }
 
   describe("Types recognition", function() {
+    afterEach(function() { remove(); });
+    it("should create a CTS.Service object", function() {
+      startFake();
+      expect(instance.service).toBeDefined();
+      expect(instance.service.getValues).toBeDefined()
+    })
     it("should add checkbox", function() {
       startFake();
       expect(fixture.find("input[type='checkbox']").length).toEqual(1);
-      remove(); 
     });
     it("should add hidden", function() {
       startFake();
       expect(fixture.find("input[type='hidden']").length).toEqual(1);
-      remove(); 
     });
     it("should add input[text]", function() {
       startFake();
       expect(fixture.find("input[type='text']").length).toEqual(2);
-      remove(); 
     });
     it("should add textarea", function() {
       startFake();
       expect(fixture.find("textarea").length).toEqual(1);
-      remove(); 
     });
   });
 
   describe("Default recognition", function() {
+    afterEach(function() { remove(); });
     it("should show true on hidden", function() {
       start("fakeService");
       expect(fixture.find("input[type='hidden']").val()).toEqual("true");
-      remove();
     });
     it("should show false on hidden", function() {
       start("fakeService2");
       expect(fixture.find("input[type='hidden']").val()).toEqual("false");
-      remove();
     });
     it("should check on true for checkbox", function() {
       start("fakeService2");
       expect(fixture.find("input[type='checkbox']:checked").length).toEqual(1);
-      remove();
     });
     it("should not check on false for checkbox", function() {
       startFake();
       expect(fixture.find("input[type='checkbox']:checked").length).toEqual(0);
-      remove();
     });
     it("should put nothing when default is empty", function() {
       startFake();
       expect(fixture.find("textarea").val()).toEqual("");
-      remove();
     });
     it("should put nothing when default is empty", function() {
       startFake();
       expect(fixture.find("input[type='text']").last().val()).toEqual("TEI");
-      remove();
     });
   });
 
   describe("GetValues recognition", function() {
+    afterEach(function() { remove(); });
+    it("should retrieve right boolean for checkbox", function() {
+      startFake();
+      expect(instance.getValues().checkbox).toEqual(false);
+      instance.inputs.checkbox[0].checked = true;
+      expect(instance.getValues().checkbox).toEqual(true);
+    });
+    it("should retrieve right boolean for hidden", function() {
+      startFake();
+      expect(instance.getValues().boolean).toEqual(true);
+      instance.inputs.boolean.val(false);
+      expect(instance.getValues().boolean).toEqual(false);
+    });
+    it("should retrieve text for text", function() {
+      startFake();
+      //With \n for textarea
+      instance.inputs.textarea.val("Hello there\n ahah !");
+      expect(instance.getValues().textarea).toEqual("Hello there\n ahah !");
+      //Without for input
+      instance.inputs.input.val("Hello there\n ahah !");
+      expect(instance.getValues().input.replace("  ", " ")).toEqual("Hello there ahah !"); // Phantom replace \n by \s so let trim this string
+    });
+    it("should retrieve list for list", function() {
+      startFake();
+      expect(instance.getValues().list).toEqual(["teiHeader","head","speaker","note","ref"]);
+      instance.inputs.list.val("ahah");
+      expect(instance.getValues().list).toEqual(["ahah"]);
+      instance.inputs.list.val("ahah, ohoh");
+      expect(instance.getValues().list).toEqual(["ahah", "ohoh"]);
+    })
+  });
+
+  describe("Workflow", function() {
+    afterEach(function() { remove(); });
+    it("should send to the right endpoint", function() {
+      startFake({
+        "endpoint" : "http://services.perseids.org/llt/segtok",
+        trigger : "trigger"
+      });
+      fixture.trigger("trigger")
+      expect(jasmine.Ajax.requests.mostRecent().url).toEqual("http://services.perseids.org/llt/segtok")
+    });
+    it("should call the callback", function() {
+      var spy = jasmine.createSpy("success");
+      startFake({
+        callback : spy,
+        trigger : "trigger"
+      });
+      fixture.trigger("trigger");
+      jasmine.Ajax.requests.mostRecent().respondWith({
+        status : 200
+      })
+      expect(spy).toHaveBeenCalled();
+      spy = null;
+    });
+    it("should trigger doing", function() {
+      var spy = jasmine.createSpy("success");
+      startFake({
+        trigger : "trigger"
+      });
+      fixture.on("cts-service:fakeService:doing", spy)
+      fixture.trigger("trigger");
+      expect(spy).toHaveBeenCalled();
+      spy = null;
+    });
+    it("should trigger done", function() {
+      var spy = jasmine.createSpy("success");
+      startFake({
+        trigger : "trigger"
+      });
+      fixture.on("cts-service:fakeService:done", spy)
+      fixture.trigger("trigger");
+      jasmine.Ajax.requests.mostRecent().respondWith({
+        status : 200
+      })
+      expect(spy).toHaveBeenCalled();
+      spy = null;
+    });
+    it("should run on click", function() {
+      var spy = jasmine.createSpy("success");
+      var element = $j("<a />");
+      startFake({
+        click : element,
+        callback : spy
+      });
+      element.trigger("click")
+      //Need to answer to have callback
+      jasmine.Ajax.requests.mostRecent().respondWith({
+        status : 200
+      })
+      expect(spy).toHaveBeenCalled();
+      spy = null;
+    });
+    it("should pass data", function() {
+      var element = $j("<a />");
+      startFake({
+        click : element
+      });
+      element.trigger("click")
+      //Need to answer to have callback
+      expect(jasmine.Ajax.requests.mostRecent().params.length).toBeGreaterThan(0);
+    });
+  });
+
+  describe("Driver options", function() {
+
+  });
+
+  describe("Names options", function() {
+    
+  });
+
+  describe("CSS/show options", function() {
+    
+  });
+
+  describe("Defaults options", function() {
+    
   });
 });
