8389a30e2e62d280d4e7ed56c83cb87151e80f8a
Thibault Clerice <leponteineptique@gmail.com>
2015-02-11 17:15:10 +0100

    Added final test for E2E Service

--- spec/e2e/plugin.jquery.cts.service.js
+++ spec/e2e/plugin.jquery.cts.service.js
@@ -281,18 +281,188 @@ describe('jQuery CTS Selector', function() {
   });
 
   describe("Driver options", function() {
-
+    afterEach(remove);
+    it("should take element for values", function() {
+      var a = $j("<input />", { type : "text"});
+      startFake({
+        DOM : {
+          "input" : a
+        }
+      });
+      expect(instance.getValues().input).toEqual("")
+      a.val("foo")
+      expect(instance.getValues().input).toEqual("foo")
+    });
+    it("should take selector for values", function() {
+      var a = $j("<input />", { type : "text", "class" : "element"});
+      startFake({
+        DOM : {
+          "input" : ".element"
+        }
+      });
+      fixture.append(a)
+      expect(instance.getValues().input).toEqual("")
+      a.val("bar")
+      expect(instance.getValues().input).toEqual("bar")
+    })
+    it("should take callback for value", function() {
+      startFake({
+        DOM : {
+          "input" : function() { return "foo"; }
+        }
+      });
+      expect(instance.getValues().input).toEqual("foo")
+    })
+    it("should take list", function() {
+      startFake({
+        DOM : {
+          "list" : function() { return ["foo"]; }
+        }
+      });
+      expect(instance.getValues().list).toEqual(["foo"])
+    });
+    it("should take boolean", function() {
+      var foo = function() { return true; }
+      startFake({
+        DOM : {
+          "boolean" : function() { return foo(); }
+        }
+      });
+      expect(instance.getValues().boolean).toEqual(true)
+      var foo = function() { return false; }
+      expect(instance.getValues().boolean).toEqual(false)
+    });
+    it("should handle checkbox selector correctly", function() {
+      var a = $j("<input />", { type : "checkbox", "class" : "element"});
+      a[0].checked = true;
+      startFake({
+        DOM : {
+          "checkbox" : ".element"
+        }
+      });
+      fixture.append(a)
+      expect(instance.getValues().checkbox).toEqual(true)
+      a[0].checked = false;
+      expect(instance.getValues().checkbox).toEqual(false)
+    });
+    it("should handle checkbox object correctly", function() {
+      var a = $j("<input />", { type : "checkbox", "class" : "element"});
+      a[0].checked = true;
+      startFake({
+        DOM : {
+          "boolean" : a
+        }
+      });
+      fixture.append(a)
+      expect(instance.getValues().boolean).toEqual(true)
+      a[0].checked = false;
+      expect(instance.getValues().boolean).toEqual(false)
+    });
   });
 
   describe("Names options", function() {
-    
+    //The name option give names to fields
+    //Our goal here is to check it works with every type of input supported
+    afterEach(remove);
+    it("should work for checkbox", function() {
+      startFake({
+        names : {
+          "checkbox" : "boolean"
+        }
+      });
+      expect(fixture.find("input[type='checkbox'][name='boolean']").length).toEqual(1)
+    })
+    it("should work for hidden", function() {
+      startFake({
+        names : {
+          "boolean" : "boolean"
+        }
+      });
+      expect(fixture.find("input[type='hidden'][name='boolean']").length).toEqual(1)
+    })
+    it("should work for text", function() {
+      startFake({
+        names : {
+          "input" : "boolean"
+        }
+      });
+      expect(fixture.find("input[type='text'][name='boolean']").length).toEqual(1)
+    })
+    it("should work for text", function() {
+      startFake({
+        names : {
+          "textarea" : "boolean"
+        }
+      });
+      expect(fixture.find("textarea[name='boolean']").length).toEqual(1)
+    })
   });
 
   describe("CSS/show options", function() {
-    
+    afterEach(remove);
+    it("shoud not show on show=false", function() {
+      startFake({show:false});
+      expect(fixture.find("fieldset:visible").length).toEqual(0)
+    })
+    it("shoud not show on show=true", function() {
+      startFake({show:true});
+      expect(fixture.find("fieldset:visible").length).toEqual(1)
+    })
+    it("shoud toggle on trigger on target element", function() {
+      startFake({show:"true"});
+
+      expect(fixture.find("fieldset:visible").length).toEqual(1)
+      fixture.trigger("true");
+      expect(fixture.find("fieldset:visible").length).toEqual(0)
+    });
+
+    it("should use css defined class", function() {
+      startFake({
+        css : {
+          "container" : ["container"],
+          "container-legend" : ["container-legend"],
+
+          "field-container" : ["field-container"],
+          "field-label" : ["field-label"],
+          "field-input-container" : ["field-input-container"],
+
+          "field-text" : ["field-text"],
+          "field-checkbox" : ["field-checkbox"],
+          "field-textarea" : ["field-textarea"]
+        }
+      })
+      expect(fixture.find(".container").length).toEqual(1);
+      expect(fixture.find(".container-legend").length).toEqual(1);
+
+      expect(fixture.find(".field-textarea").length).toEqual(1);
+      expect(fixture.find(".field-text").length).toEqual(2); // list + input
+      expect(fixture.find(".field-checkbox").length).toEqual(1);
+
+      //There is one hidden so... 4 fields !
+      expect(fixture.find(".field-container").length).toEqual(4)
+      expect(fixture.find(".field-label").length).toEqual(4)
+      expect(fixture.find(".field-input-container").length).toEqual(4)
+    })
+
   });
 
   describe("Defaults options", function() {
-    
+    afterEach(remove);
+    it("should take boolean default", function() {
+      startFake({
+        "defaults" : {
+          "checkbox" : true,
+          "boolean" : false,//hidden
+          "textarea" : "hello",
+          "list" : ["a", "b"],
+          "input" : "Epidoc"
+        }
+      });
+      expect(instance.getValues().checkbox).toEqual(true);
+      expect(instance.getValues().boolean).toEqual(false);
+      expect(instance.getValues().textarea).toEqual("hello");
+      expect(instance.getValues().list).toEqual(["a", "b"]);
+      expect(instance.getValues().input).toEqual("Epidoc");
+    });
   });
 });
