c5701e4a8ef07c34f87d6778f064b179497dcef6
Thibault Clérice <leponteineptique@gmail.com>
2015-02-17 12:35:20 +0100

    Release

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    clean: ["./build"],
     uglify: {
       options : {
         //beautify : true -> Need to figure out this stuff
@@ -202,6 +203,7 @@ module.exports = function(grunt) {
   });
 
   // Register tasks.
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
@@ -220,4 +222,6 @@ module.exports = function(grunt) {
   grunt.registerTask('test', ['jasmine', 'karma']);
   grunt.registerTask('doc', ['markdown', 'replace', 'jsdoc'])
   grunt.registerTask('page', ['doc', 'gh-pages'])
+
+  grunt.registerTask('rel', ['build', 'clean', 'release']);
 };
