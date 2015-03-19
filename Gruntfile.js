6e898f5f305e94045c28ed2a6bd60a1000bf4aca
Thibault Clérice <leponteineptique@gmail.com>
2015-03-19 15:28:07 +0100

    Removed plugins from man repo

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,26 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    clean: ["./build"],
+    clean: ["./dist"],
     uglify: {
       options : {
         //beautify : true -> Need to figure out this stuff
       },
       all: {
         files: {
-          'build/cts.min.js': ['build/cts.js'],
-          'build/jquery.cts.selector.min.js': ['build/jquery.cts.selector.js'],
-          'build/jquery.cts.typeahead.min.js': ['build/jquery.cts.typeahead.js'],
-          'build/jquery.cts.service.min.js': ['build/jquery.cts.service.js'],
-          'build/jquery.cts.xslt.min.js': ['build/jquery.cts.xslt.js']
+          'dist/cts.min.js': ['dist/cts.js']
         }
       },
       i18n: {
         files : {
-          'build/i18n/en.min.js' : ["src/i18n/en.js"]
+          'dist/i18n/en.min.js' : ["src/i18n/en.js"]
         }
       }
     },
     concat: {
       all: {
         files : {
-          'build/cts.js' : [
+          'dist/cts.js' : [
             'src/cts.js', 
             'src/modules/utils.js', 
             'src/modules/service.js', 
@@ -37,11 +33,7 @@ module.exports = function(grunt) {
             'src/endpoints/*.js', 
             'src/xslt/*.js',
             'src/i18n/en.js'
-          ],
-          'build/jquery.cts.selector.js' : ['src/plugins/jquery.cts.selector.js'],
-          'build/jquery.cts.typeahead.js' : ['src/plugins/jquery.cts.typeahead.js'],
-          'build/jquery.cts.service.js' : ['src/plugins/jquery.cts.service.js'],
-          'build/jquery.cts.xslt.js' : ['src/plugins/jquery.cts.xslt.js']
+          ]
         }
       }
     },
@@ -69,37 +61,6 @@ module.exports = function(grunt) {
         keepRunner : true
       }
     },
-    karma: {
-      unit: {
-        configFile: 'karma.conf.js',
-        options : {
-          singleRun : true,
-          browsers : ["PhantomJS"]
-        }
-      }
-    },
-    markdown: {
-      all: {
-        files: [
-          {src: ['README.md'], dest: 'index.html'},
-          {src: ['./doc/i18n.md'], dest: 'doc_html/i18n.html'},
-          {src: ['./doc/plugins/jquery.cts.selector.md'],  dest: 'doc_html/plugins.jquery.cts.selector.html'},
-          {src: ['./doc/plugins/jquery.cts.typeahead.md'], dest: 'doc_html/plugins.jquery.cts.typeahead.html'},
-          {src: ['./doc/plugins/jquery.cts.xslt.md'],      dest: 'doc_html/plugins.jquery.cts.xslt.html'},
-          {src: ['./doc/plugins/jquery.cts.service.md'],   dest: 'doc_html/plugins.jquery.cts.service.html'},
-
-          {src: ['./doc/services/llt.tokenizer.md'],       dest: 'doc_html/services.llt.tokenizer.html'},
-          {src: ['./doc/services/new.md'],                 dest: 'doc_html/services.new.html'},
-
-          {src: ['./doc/xslt/llt.segtok_to_tb.md'],        dest: 'doc_html/xslt.llt.segtok_to_tb.html'},
-          {src: ['./doc/xslt/new.md'],                     dest: 'doc_html/xslt.new.html'}
-
-        ],
-        options : {
-          template : "./doc/template/template.html"
-        }
-      }
-    },
     jsdoc : {
       dist : {
         src: [
@@ -107,98 +68,19 @@ module.exports = function(grunt) {
           'src/modules/*.js'
           ],
         options: {
-          destination: 'doc_html/api',
+          destination: 'doc_html',
           configure : "jsdoc.conf.json",
           template: './node_modules/grunt-jsdoc/node_modules/ink-docstrap/template'
         }
       }
     },
-    replace: {
-      index : {
-        src : './index.html', 
-        dest : 'index.html',
-        replacements: [{
-          from: 'doc/plugins/',                   // string replacement
-          to: '/doc_html/plugins.'
-        },{
-          from: 'doc/services/',                   // string replacement
-          to: '/doc_html/services.'
-        },{
-          from: 'doc/xslt/',                   // string replacement
-          to: '/doc_html/xslt.'
-        },{
-          from: '../plugins/',                   // string replacement
-          to: '/doc_html/plugins.'
-        },{
-          from: '../services/',                   // string replacement
-          to: '/doc_html/services.'
-        },{
-          from: '../xslt/',                   // string replacement
-          to: '/doc_html/xslt.'
-        },{
-          from: 'doc/',                   // string replacement
-          to: '/doc_html/'
-        },{
-          from: 'README.md',                   // string replacement
-          to: '/index.html'
-        },{
-          from: '../',                   // string replacement
-          to: ''
-        },{
-          from: '.md',                   // string replacement
-          to: '.html'
-        },{
-          from: '"/',                   // string replacement
-          to: '"/Capitains-Sparrow/'
-        }]
-      },
-      htmlpages: {
-        src : './doc_html/*.html',
-        dest : './doc_html/',
-        replacements: [{
-          from: 'doc/plugins/',                   // string replacement
-          to: 'plugins.'
-        },{
-          from: 'doc/services/',                   // string replacement
-          to: 'services.'
-        },{
-          from: 'doc/xslt/',                   // string replacement
-          to: 'xslt.'
-        },{
-          from: '../plugins/',                   // string replacement
-          to: 'plugins.'
-        },{
-          from: '../services/',                   // string replacement
-          to: 'services.'
-        },{
-          from: '../xslt/',                   // string replacement
-          to: 'xslt.'
-        },{
-          from: 'doc/',                   // string replacement
-          to: ''
-        },{
-          from: 'README.md',                   // string replacement
-          to: '/index.html'
-        },{
-          from: '../',                   // string replacement
-          to: ''
-        },{
-          from: '.md',                   // string replacement
-          to: '.html'
-        },{
-          from: 'src/',                   // string replacement
-          to: 'https://github.com/PerseusDL/Capitains-Sparrow/blob/master/src/'
-        },{
-          from: '/https',                   // string replacement
-          to: 'https'
-        },{
-          from: '"/',                   // string replacement
-          to: '"/Capitains-Sparrow/'
-        }]
-      }
-    },
     'gh-pages': {
-      src: ['**']
+      src: ['doc_html']
+    }
+    release : {
+      options: {
+        additionalFiles: ['bower.json']
+      }
     }
   });
 
@@ -207,21 +89,19 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
+
   grunt.loadNpmTasks('grunt-jslint');
-  grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks("grunt-jsdoc");
-  grunt.loadNpmTasks("grunt-markdown");
-  grunt.loadNpmTasks('grunt-text-replace');
+
   grunt.loadNpmTasks('grunt-gh-pages');
   grunt.loadNpmTasks('grunt-release');
 
   // Default task. 
   grunt.registerTask('default', ['concat', 'uglify']);
   grunt.registerTask('build', ['default']);
-  //grunt.registerTask('jshint', ['jshint']);
-  grunt.registerTask('test', ['jasmine', 'karma']);
-  grunt.registerTask('doc', ['markdown', 'replace', 'jsdoc'])
-  grunt.registerTask('page', ['doc', 'gh-pages'])
 
-  grunt.registerTask('rel', ['build', 'release']);
+  grunt.registerTask('test', ['jasmine']);
+  grunt.registerTask('page', ['jsdoc', 'gh-pages'])
+
+  grunt.registerTask('version', ['build', 'release', 'page']);
 };
