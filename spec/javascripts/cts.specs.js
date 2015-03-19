6e214be480eceec40e37eeb648f947c9dcb80ce1
Thibault Clérice <leponteineptique@gmail.com>
2015-03-19 15:45:59 +0100

    Updates

--- spec/javascripts/cts.specs.js
+++
@@ -1,9 +0,0 @@
-describe( "Testing CTS Global object", function () {
-
-    describe("Verifying its modules are available", function() {
-        it("should exist and be an object", function() {
-            expect(typeof CTS).toEqual("object");
-        });
-    });
-
-});
